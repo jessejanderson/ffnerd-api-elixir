f5dd739c26c070b6421be18f4a7ae884a4cfcf93
Jesse J. Anderson <jessejanderson@gmail.com>
2016-06-09 17:35:05 -0700

    add docs for Team

--- lib/ffnerd/team.ex
+++ lib/ffnerd/team.ex
@@ -8,6 +8,15 @@ defmodule FFNerd.Team do
   use ExConstructor
   use HTTPoison.Base
 
+  @doc """
+  List teams
+
+  ## Examples
+
+    FFNerd.Team.list client
+
+  More info at: http://www.fantasyfootballnerd.com/fantasy-football-api#teams
+  """
   def list(client) do
     FFNerd.get({:NFLTeams, %FFNerd.Url{service: "nfl-teams"}}, client)
     |> Enum.map(&new/1)
