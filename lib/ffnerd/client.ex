0904af26a5112d82fa9980aeab267a4d4f09dad6
Jesse J. Anderson <jessejanderson@gmail.com>
2016-06-09 17:29:48 -0700

    add docs to Client

--- lib/ffnerd/client.ex
+++ lib/ffnerd/client.ex
@@ -1,9 +1,23 @@
 defmodule FFNerd.Client do
   defstruct auth: nil, endpoint: "http://www.fantasyfootballnerd.com"
 
+  @doc """
+  Create new client with default endpoint.
+
+  ## Examples
+
+    FFNerd.Client.new "API-KEY-ABC-XYZ"
+
+  """
   def new(auth), do: %__MODULE__{auth: auth}
 
-  def new(auth, endpoint) do
-    %__MODULE__{auth: auth, endpoint: endpoint}
-  end
+  @doc """
+  Create new client with custom endpoint.
+
+  ## Examples
+
+    FFNerd.Client.new "API-KEY", "http://www.example.com"
+
+  """
+  def new(auth, endpoint), do: %__MODULE__{auth: auth, endpoint: endpoint}
 end
