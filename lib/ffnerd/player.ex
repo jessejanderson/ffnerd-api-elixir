6910f91796452c809f1a260eff36d6a734f288c2
Jesse J. Anderson <jessejanderson@gmail.com>
2016-06-09 17:26:31 -0700

    add docs to Player

--- lib/ffnerd/player.ex
+++ lib/ffnerd/player.ex
@@ -15,17 +15,46 @@ defmodule FFNerd.Player do
             twitter_id: nil
   use ExConstructor
   use HTTPoison.Base
+  alias FFNerd.Client
 
+  @doc """
+  List players
+
+  ## Examples
+
+    FFNerd.Player.list client
+
+  More info at: http://www.fantasyfootballnerd.com/fantasy-football-api#players
+  """
   def list(client) do
     FFNerd.get({:Players, %FFNerd.Url{service: "players"}}, client)
     |> Enum.map(&new/1)
   end
 
+  @doc """
+  List players of a specified position
+
+  ## Examples
+
+    FFNerd.Player.list "QB", client
+
+  More info at: http://www.fantasyfootballnerd.com/fantasy-football-api#players
+  """
   def list(position, client) do
     FFNerd.get({:Players, %FFNerd.Url{service: "players", path1: position}}, client)
     |> Enum.map(&new/1)
   end
 
+
+  @doc """
+  Find a player by their id
+
+  ## Examples
+
+    FFNerd.Player.find 2, client
+
+  More info at: http://www.fantasyfootballnerd.com/fantasy-football-api#players
+  """
   def find(id, client) do
     FFNerd.get({:Player, %FFNerd.Url{service: "player", path1: id}}, client)
     |> new
