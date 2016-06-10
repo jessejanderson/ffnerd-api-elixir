2ec5ee667b265d3aa83149dbf60d05af28d6424c
Jesse J. Anderson <jessejanderson@gmail.com>
2016-06-09 17:33:56 -0700

    add docs to Schedule and Game

--- lib/ffnerd/game.ex
+++ lib/ffnerd/game.ex
@@ -9,8 +9,26 @@ defmodule FFNerd.Game do
   use ExConstructor
   use HTTPoison.Base
 
+  @doc """
+  List games
+
+  ## Examples
+
+    FFNerd.Game.list client
+
+  More info at: http://www.fantasyfootballnerd.com/fantasy-football-api#schedule
+  """
   def list(client), do: FFNerd.Schedule.list(client)
 
+  @doc """
+  Find a game by id
+
+  ## Examples
+
+    FFNerd.Game.find 2, client
+
+  More info at: http://www.fantasyfootballnerd.com/fantasy-football-api#schedule
+  """
   def find(id, client) do
     FFNerd.get({:Schedule, %FFNerd.Url{service: "schedule"}}, client)
     |> Enum.filter(fn(%{"gameId" => game_id}) -> game_id == "#{id}" end)
--- lib/ffnerd/schedule.ex
+++ lib/ffnerd/schedule.ex
@@ -2,16 +2,29 @@ defmodule FFNerd.Schedule do
   alias FFNerd.Game
   use HTTPoison.Base
 
+  @doc """
+  List schedule
+
+  ## Examples
+
+    FFNerd.Schedule.list client
+
+  More info at: http://www.fantasyfootballnerd.com/fantasy-football-api#schedule
+  """
   def list(client) do
     FFNerd.get({:Schedule, %FFNerd.Url{service: "schedule"}}, client)
     |> Enum.map(&Game.new/1)
   end
 
-  def list(position, client) do
-    FFNerd.get({:Schedule, %FFNerd.Url{service: "schedule", path1: position}}, client)
-    |> Enum.map(&Game.new/1)
-  end
+  @doc """
+  List current week
 
+  ## Examples
+
+    FFNerd.Schedule.current_week client
+
+  More info at: http://www.fantasyfootballnerd.com/fantasy-football-api#schedule
+  """
   def current_week(client) do
     FFNerd.get({:currentWeek, %FFNerd.Url{service: "schedule"}}, client)
   end
